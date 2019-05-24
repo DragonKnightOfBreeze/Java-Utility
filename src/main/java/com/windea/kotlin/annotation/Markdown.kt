1765b9158c17e0d72f21084423994f9cbc7f55c3
微风的龙骑士 风游迩 <2944262241@qq.com>
2019-05-24 20:27:00 +0800

    更新脚本

--- src/main/java/com/windea/kotlin/annotation/Markdown.kt
+++ src/main/java/com/windea/kotlin/annotation/Markdown.kt
@@ -3,6 +3,7 @@ package com.windea.kotlin.annotation
 /**
  * Markdown附加注释的注解。
  */
+@Repeatable
 annotation class Markdown(
 	val text: String,
 	val useExtend: Boolean = false,
--- src/main/java/com/windea/kotlin/annotation/Tested.kt
+++ src/main/java/com/windea/kotlin/annotation/Tested.kt
@@ -3,6 +3,7 @@ package com.windea.kotlin.annotation
 /**
  * 已通过测试的代码的注解。
  */
+@MustBeDocumented
 annotation class Tested(
 	val message: String = "tested"
 )
